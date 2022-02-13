cbd86f8d86d0ab531390760f04b397a492e56979
Assaf Morami <assaf.morami@gmail.com>
2022-02-13 15:19:15 +0200

    Webpack?

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./encryption";
 export * from "./query";
 export * from "./secret_network_client";
-export * from "./wallet";
 export * from "./tx";
+export * from "./wallet";
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
@@ -10,17 +11,30 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
-      {
-        test: /\.js$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
-      },
     ],
   },
+  plugins: [
+    new NodePolyfillPlugin(),
+
+    // new webpack.DefinePlugin({
+    //   XMLHttpRequest: require("xhr2"),
+    // }),
+  ],
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: [".js", ".ts"],
+    // fallback: {
+    //   // util: require.resolve("util"),
+    //   // assert: require.resolve("assert"),
+    //   buffer: require.resolve("buffer"),
+    //   crypto: require.resolve("crypto-browserify"),
+    //   // http: require.resolve("stream-http"),
+    //   // https: require.resolve('https-browserify'),
+    //   // os: require.resolve("os-browserify/browser"),
+    //   path: require.resolve("path-browserify"),
+    //   stream: require.resolve("stream-browserify"),
+    //   // zlib: require.resolve("browserify-zlib"),
+    // },
   },
-  plugins: [new NodePolyfillPlugin()],
   mode: "production",
   output: {
     library: "secretjs",
