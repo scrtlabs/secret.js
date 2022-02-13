e9a87d6881237378ca361c6c6421d4a50da20624
Assaf Morami <assaf.morami@gmail.com>
2022-02-13 12:21:17 +0200

    Try webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new NodePolyfillPlugin()],
+  mode: "production",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
