a2b968dc36888704f4e6bdab0aa969cb9fd7836a
Assaf Morami <assaf.morami@gmail.com>
2022-02-15 17:20:25 +0200

    MsgTransfer

--- src/tx/ibc_transfer.ts
+++ src/tx/ibc_transfer.ts
@@ -1,15 +1,92 @@
-import { AminoMsg, Msg, ProtoMsg } from "./types";
+import { AminoMsg, Coin, Msg, ProtoMsg } from "./types";
+
+export type MsgTransferParams = {
+  /** the port on which the packet will be sent */
+  sourcePort: string;
+  /** the channel by which the packet will be sent */
+  sourceChannel: string;
+  /** the tokens to be transferred */
+  token: Coin;
+  /** the sender address */
+  sender: string;
+  /** the recipient address on the destination chain */
+  receiver: string;
+  /**
+   * Timeout height relative to the current block height.
+   * The timeout is disabled when undefined or set to 0.
+   */
+  timeoutHeight?: Height;
+  /**
+   * Timeout timestamp (in secons) since Unix epoch.
+   * The timeout is disabled when undefined or set to 0.
+   */
+  timeoutTimestamp?: string;
+};
+
+/**
+ * Height is a monotonically increasing data type
+ * that can be compared against another Height for the purposes of updating and
+ * freezing clients
+ *
+ * Normally the RevisionHeight is incremented at each height while keeping
+ * RevisionNumber the same. However some consensus algorithms may choose to
+ * reset the height in certain conditions e.g. hard forks, state-machine
+ * breaking changes In these cases, the RevisionNumber is incremented so that
+ * height continues to be monitonically increasing even as the RevisionHeight
+ * gets reset
+ */
+export type Height = {
+  /** the revision that the client is currently on */
+  revisionNumber: string;
+  /** the height within the given revision */
+  revisionHeight: string;
+};
 
 export class MsgTransfer implements Msg {
-  constructor(
-    msg: import("../protobuf_stuff/ibc/applications/transfer/v1/tx").MsgTransfer,
-  ) {}
+  constructor(public params: MsgTransferParams) {}
 
   async toProto(): Promise<ProtoMsg> {
-    throw new Error("MsgTransfer not implemented.");
+    const msgContent = {
+      sourcePort: this.params.sourcePort,
+      sourceChannel: this.params.sourceChannel,
+      token: this.params.token,
+      sender: this.params.sender,
+      receiver: this.params.receiver,
+      timeoutHeight: this.params.timeoutHeight,
+      timeoutTimestamp: this.params.timeoutTimestamp
+        ? `${this.params.timeoutTimestamp}000000000` // sec -> ns
+        : "0",
+    };
+
+    return {
+      typeUrl: "/ibc.applications.transfer.v1.MsgTransfer",
+      value: msgContent,
+      encode: async () =>
+        (
+          await import("../protobuf_stuff/ibc/applications/transfer/v1/tx")
+        ).MsgTransfer.encode(msgContent).finish(),
+    };
   }
 
   async toAmino(): Promise<AminoMsg> {
-    throw new Error("MsgTransfer not implemented.");
+    return {
+      type: "cosmos-sdk/MsgTransfer",
+      value: {
+        source_port: this.params.sourcePort,
+        source_channel: this.params.sourceChannel,
+        token: this.params.token,
+        sender: this.params.sender,
+        receiver: this.params.receiver,
+        timeout_height: this.params.timeoutHeight
+          ? {
+              revision_number: this.params.timeoutHeight.revisionNumber,
+              revision_height: this.params.timeoutHeight.revisionHeight,
+            }
+          : undefined,
+        timeout_timestamp: this.params.timeoutTimestamp
+          ? `${this.params.timeoutTimestamp}000000000` // sec -> ns
+          : "0",
+      },
+    };
   }
 }
