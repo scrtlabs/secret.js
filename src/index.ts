7cab9406453bd508c9d16442ed8a6d87b12f7e2b
Assaf Morami <assaf.morami@gmail.com>
2022-02-21 21:48:01 +0200

    export pubkeyToAddress

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "./encryption";
 export * from "./query";
 export * from "./secret_network_client";
 export * from "./tx";
-export { WalletOptions } from "./wallet_amino";
+export { pubkeyToAddress, WalletOptions } from "./wallet_amino";
 export { Wallet } from "./wallet_proto";
