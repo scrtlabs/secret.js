f459d87934a95f295a0eb05f6e579d10dd430b17
Assaf Morami <assaf.morami@gmail.com>
2022-03-13 12:49:59 +0200

    Fix find_all_grpc-web_endpoints.sh

--- scripts/find_all_grpc-web_endpoints.sh
+++ scripts/find_all_grpc-web_endpoints.sh
@@ -2,10 +2,12 @@
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-grep -Pr 'protobufPackage|methodName' "${SCRIPT_PATH}/../src/protobuf_stuff" |
-    awk '/protobufPackage/{package = $5} /methodName/{print "/"package"/"$3}' |
+grep -Pr 'serviceName|methodName' "${SCRIPT_PATH}/../src/protobuf_stuff" |
+    awk '/serviceName/{package = $3} /methodName/{print "/"package"/"$3}' |
     sort -u |
-    grep -v reflection |
+    grep -v 'cosmos.base.reflection' |
+    grep -v 'tendermint.abci' |
     tr -d , |
     tr -d ';' |
-    tr -d '"'
+    tr -d '"' |
+    grep -P '(Service|Query)/'
