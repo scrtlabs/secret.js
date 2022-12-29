scrtlabs/secret.js

commit f44d530d77864c178ca00223da2dbe62404a9826

Regen ts with new protoc-gen-grpc-gateway-ts

src/grpc_gateway/fetch.pb.ts
@@ -337,5 +337,8 @@ export function renderURLSearchParams<T extends RequestPayload>(
     [] as string[][]
   );
 
-  return new URLSearchParams(urlSearchParams).toString();
+  //react-native doesn't like working with array of arrays, and this is easier to patch
+  const searchParamsAsObject = Object.fromEntries(urlSearchParams);
+
+  return new URLSearchParams(searchParamsAsObject).toString();
 }

scripts/generate_protobuf.sh
@@ -39,7 +39,7 @@ rm -rf "$GRPC_GATEWAY_OUT_DIR"
 mkdir -p "$GRPC_GATEWAY_OUT_DIR"
 
 # go install github.com/grpc-ecosystem/protoc-gen-grpc-gateway-ts@latest
-PLUGIN_PATH_GRPC_GATEWAY="$(which protoc-gen-grpc-gateway-ts)"
+PLUGIN_PATH_GRPC_GATEWAY="/home/bob/go/bin/protoc-gen-grpc-gateway-ts"
 
 protoc \
   --plugin="protoc-gen-grpc_gateway_ts=${PLUGIN_PATH_GRPC_GATEWAY}" \

commit 90d45b5560f935ccf1c439d5b467d54ae21a8307

Fix waitForIbcResponse() for realsies
This reverts 8c0429103673890d827c6b04c252f26885a83095 and fixes the bug that was caused by f44d530d77864c178ca00223da2dbe62404a9826 when sending an HTTP request with multiple URL search params with the same key (arrays)

src/grpc_gateway/fetch.pb.ts
@@ -337,8 +337,8 @@ export function renderURLSearchParams<T extends RequestPayload>(
     [] as string[][]
   );
 
-  //react-native doesn't like working with array of arrays, and this is easier to patch
-  const searchParamsAsObject = Object.fromEntries(urlSearchParams);
-
-  return new URLSearchParams(searchParamsAsObject).toString();
+  // react-native doesn't like working with array of arrays, and this is easier to patch
+  return urlSearchParams
+    .map((x) => new URLSearchParams({ [x[0]]: x[1] }).toString())
+    .join("&");
 }

src/secret_network_client.ts
@@ -878,27 +878,13 @@ export class SecretNetworkClient {
       while (tries > 0 && !isDoneObject.isDone) {
         const txs = await this.txsQuery(query);
 
-        const ibcResp = txs.find(
-          (x) =>
-            x.arrayLog?.find(
-              (x) =>
-                x.type === `${txType}_packet` &&
-                x.key === "packet_src_channel" &&
-                x.value === packetSrcChannel,
-            ) &&
-            x.arrayLog?.find(
-              (x) =>
-                x.type === `${txType}_packet` &&
-                x.key === "packet_sequence" &&
-                x.value === packetSequence,
-            ),
-        );
+        const ibcRespTx = txs.find((tx) => tx.code === 0);
 
-        if (ibcResp) {
+        if (ibcRespTx) {
           isDoneObject.isDone = true;
           resolve({
             type,
-            tx: ibcResp,
+            tx: ibcRespTx,
           });
         }
 