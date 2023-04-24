a14c0add020dd9dbb9b3c465551ff90dcb31552d
Eshel <eshel@scrtlabs.com>
2023-04-24 13:05:33 +0300

    generating proto for grpc gateway too (had to install a missing tool)

---
+++ src/grpc_gateway/secret/ibc-switch/v1beta1/genesis.pb.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as SecretIbcswitchV1beta1Params from "./params.pb"
+export type GenesisState = {
+  params?: SecretIbcswitchV1beta1Params.Params
+}
