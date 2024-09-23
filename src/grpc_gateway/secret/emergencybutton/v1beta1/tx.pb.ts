20f26a66ea2bcda95d427056ab23e58211adf178
Ilya Raykker <ilya.th.kapitonov@gmail.com>
2024-09-23 12:10:02 +0200

    Update proto files because of adding MsgUpdateParams call to emergency button

--- src/grpc_gateway/secret/emergencybutton/v1beta1/tx.pb.ts
+++ src/grpc_gateway/secret/emergencybutton/v1beta1/tx.pb.ts
@@ -5,6 +5,7 @@
 */
 
 import * as fm from "../../../fetch.pb"
+import * as SecretEmergencybuttonV1beta1Params from "./params.pb"
 export type MsgToggleIbcSwitch = {
   sender?: string
 }
@@ -12,8 +13,19 @@ export type MsgToggleIbcSwitch = {
 export type MsgToggleIbcSwitchResponse = {
 }
 
+export type MsgUpdateParams = {
+  authority?: string
+  params?: SecretEmergencybuttonV1beta1Params.Params
+}
+
+export type MsgUpdateParamsResponse = {
+}
+
 export class Msg {
   static ToggleIbcSwitch(req: MsgToggleIbcSwitch, initReq?: fm.InitReq): Promise<MsgToggleIbcSwitchResponse> {
     return fm.fetchReq<MsgToggleIbcSwitch, MsgToggleIbcSwitchResponse>(`/secret.emergencybutton.v1beta1.Msg/ToggleIbcSwitch`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static UpdateParams(req: MsgUpdateParams, initReq?: fm.InitReq): Promise<MsgUpdateParamsResponse> {
+    return fm.fetchReq<MsgUpdateParams, MsgUpdateParamsResponse>(`/secret.emergencybutton.v1beta1.Msg/UpdateParams`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
   }
 }
--- src/tx/emergency_button.ts
+++ src/tx/emergency_button.ts
@@ -1,9 +1,15 @@
 import { Msg, AminoMsg, ProtoMsg } from "./types";
-import { MsgToggleIbcSwitch as MsgToggleIbcSwitchParams } from "../protobuf/secret/emergencybutton/v1beta1/tx";
+import {
+  MsgToggleIbcSwitch as MsgToggleIbcSwitchParams,
+  MsgUpdateParams as MsgUpdateParamsParams,
+} from "../protobuf/secret/emergencybutton/v1beta1/tx";
 
-export { MsgToggleIbcSwitch as MsgToggleIbcSwitchParams } from "../protobuf/secret/emergencybutton/v1beta1/tx";
+export {
+  MsgToggleIbcSwitch as MsgToggleIbcSwitchParams,
+  MsgUpdateParams as MsgUpdateParamsParams,
+} from "../protobuf/secret/emergencybutton/v1beta1/tx";
+export { Params as EmergencyButtonParams } from "../protobuf/secret/emergencybutton/v1beta1/params";
 
-/** MsgSend represents a message to send coins from one account to another. */
 export class MsgToggleIbcSwitch implements Msg {
   constructor(private params: MsgToggleIbcSwitchParams) {}
 
@@ -18,6 +24,25 @@ export class MsgToggleIbcSwitch implements Msg {
   async toAmino(): Promise<AminoMsg> {
     return {
       type: "emergencybutton/MsgToggleIbcSwitch",
+      value: this.params,
+    };
+  }
+}
+
+export class MsgUpdateParams implements Msg {
+  constructor(private params: MsgUpdateParamsParams) {}
+
+  async toProto(): Promise<ProtoMsg> {
+    return {
+      type_url: "/secret.emergencybutton.v1beta1.MsgUpdateParams",
+      value: this.params,
+      encode: () => MsgUpdateParamsParams.encode(this.params).finish(),
+    };
+  }
+
+  async toAmino(): Promise<AminoMsg> {
+    return {
+      type: "emergencybutton/MsgUpdateParams",
       value: this.params,
     };
   }
